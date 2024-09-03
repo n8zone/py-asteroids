fee483bd0cac97451d9febc29ece1fec20fd834c
Nathan <92413483+n8zone@users.noreply.github.com>
2024-09-03 10:40:13 -0400

    CircleShape base class

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	clock = pygame.time.Clock()
+	dt = 0
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -12,6 +15,7 @@ def main():
 			
 		pygame.Surface.fill(screen, (255, 255, 0))
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
